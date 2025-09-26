1161b07f6dd5cd62d26b0d055844b6966ff435e5
weiweimaodetuzi <18702136026@163.com>
2025-09-26 16:03:49 +0800

    Add files via upload

--- family-groups.js
+++ family-groups.js
@@ -6,7 +6,7 @@ const supabase = createClient(
   process.env.SUPABASE_ANON_KEY
 )
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // 设置CORS头
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
